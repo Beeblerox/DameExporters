0fede9aa97e943aca315578aa545569d6df91b37
Alexander Hohlov <zaphy@yandex.ru>
2012-06-17 20:02:01 +0400

    Started work on simple exporter

--- flixelSimple.lua
+++ flixelSimple.lua
@@ -20,8 +20,6 @@ levelName = as3.tolua(VALUE_LevelName)
 GamePackage = as3.tolua(VALUE_GamePackage)
 csvDir = as3.tolua(VALUE_CSVDir)
 importsText = as3.tolua(VALUE_Imports)
--- Version can be "2.43" or "2.5"
-flixelVersion = as3.tolua(VALUE_FlixelVersion)
 
 -- This is the file for the map base class
 baseFileText = "";
@@ -38,22 +36,28 @@ end
 
 -- This is the file for the map level class.
 fileText = "//Code generated with DAME. http://www.dambots.com\n\n"
-fileText = fileText.."package "..GamePackage.."\n"
-fileText = fileText.."{\n"
-fileText = fileText..tab1.."import "..flixelPackage..".*;\n"
+fileText = fileText.."package "..GamePackage..";\n"
+
+fileText = fileText.."//Import general Flixel classes\n"
+fileText = fileText.."import org.flixel.FlxObject;\n"
+fileText = fileText.."import org.flixel.FlxGroup;\n"
+fileText = fileText.."import org.flixel.FlxG;\n"
+fileText = fileText.."import org.flixel.FlxTilemap;\n"
+fileText = fileText.."import org.flixel.FlxSprite;\n"
+
 if # importsText > 0 then
-	fileText = fileText..tab1.."// Custom imports:\n"..importsText.."\n"
+	fileText = fileText.."// Custom imports:\n"..importsText.."\n"
 end
-fileText = fileText..tab1.."public class Level_"..levelName.." extends "..baseClassName.."\n"
-fileText = fileText..tab1.."{\n"
-fileText = fileText..tab2.."//Embedded media...\n"
+fileText = fileText.."class Level_"..levelName.." extends "..baseClassName.."\n"
+fileText = fileText.."{\n"
+fileText = fileText..tab1.."// Embedded media...\n"
 
 maps = {}
 spriteLayers = {}
 
 masterLayerAddText = ""
-stageAddText = tab3.."if ( addToStage )\n"
-stageAddText = stageAddText..tab3.."{\n"
+stageAddText = tab2.."if ( addToStage )\n"
+stageAddText = stageAddText..tab2.."{\n"
 
 for groupIndex = 0,groupCount do
 	group = groups[groupIndex]
@@ -82,21 +86,16 @@ for groupIndex = 0,groupCount do
 			if exportOnlyCSV == false then
 				table.insert(maps,{layer,layerName})
 				-- For maps just generate the Embeds needed at the top of the class.
-				fileText = fileText..tab2.."[Embed(source=\""..as3.tolua(DAME.GetRelativePath(as3Dir, csvDir.."/"..mapFileName)).."\", mimeType=\"application/octet-stream\")] public var CSV_"..layerName..":Class;\n"
-				fileText = fileText..tab2.."[Embed(source=\""..as3.tolua(DAME.GetRelativePath(as3Dir, layer.imageFile)).."\")] public var Img_"..layerName..":Class;\n"
-				masterLayerAddText = masterLayerAddText..tab3.."masterLayer.add(layer"..layerName..");\n"
+				fileText = fileText..tab1.."[Embed(source=\""..as3.tolua(DAME.GetRelativePath(as3Dir, csvDir.."/"..mapFileName)).."\", mimeType=\"application/octet-stream\")] public var CSV_"..layerName..":Class;\n"
+				fileText = fileText..tab1.."[Embed(source=\""..as3.tolua(DAME.GetRelativePath(as3Dir, layer.imageFile)).."\")] public var Img_"..layerName..":Class;\n"
+				masterLayerAddText = masterLayerAddText..tab2.."masterLayer.add(layer"..layerName..");\n"
 			end
 	
 		elseif as3.tolua(layer.IsSpriteLayer()) == true then
 			if exportOnlyCSV == false then
 				table.insert( spriteLayers,{groupName,layer,layerName})
-				masterLayerAddText = masterLayerAddText..tab3.."masterLayer.add("..layerName.."Group);\n"
-				
-				if flixelVersion ~= "2.5" then
-					masterLayerAddText = masterLayerAddText..tab3..layerName.."Group.scrollFactor.x = "..string.format("%.6f",as3.tolua(layer.xScroll))..";\n"
-					masterLayerAddText = masterLayerAddText..tab3..layerName.."Group.scrollFactor.y = "..string.format("%.6f",as3.tolua(layer.yScroll))..";\n"
-				end
-				stageAddText = stageAddText..tab4.."addSpritesForLayer"..layerName.."(onAddSpritesCallback);\n"
+				masterLayerAddText = masterLayerAddText..tab2.."masterLayer.add("..layerName.."Group);\n"
+				stageAddText = stageAddText..tab3.."addSpritesForLayer"..layerName.."(onAddSpritesCallback);\n"
 			end
 		end
 	end
@@ -105,33 +104,32 @@ end
 
 	
 if exportOnlyCSV == false then
-	stageAddText = stageAddText..tab4.."FlxG.state.add(masterLayer);\n"
-	stageAddText = stageAddText..tab3.."}\n\n"
+	stageAddText = stageAddText..tab3.."FlxG.state.add(masterLayer);\n"
+	stageAddText = stageAddText..tab2.."}\n\n"
 	
 	baseFileText = "//Code generated with DAME. http://www.dambots.com\n\n"
-	baseFileText = baseFileText.."package "..GamePackage.."\n"
+	baseFileText = baseFileText.."package "..GamePackage..";\n"
+	baseFileText = baseFileText.."import "..flixelPackage..".*;\n"
+	baseFileText = baseFileText.."class "..baseClassName.."\n"
 	baseFileText = baseFileText.."{\n"
-	baseFileText = baseFileText..tab1.."import "..flixelPackage..".*;\n"
-	baseFileText = baseFileText..tab1.."public class "..baseClassName.."\n"
-	baseFileText = baseFileText..tab1.."{\n"
 
 	fileText = fileText.."\n"
-	fileText = fileText..tab2.."//Tilemaps\n"
+	fileText = fileText..tab1.."//Tilemaps\n"
 	for i,v in ipairs(maps) do
-		fileText = fileText..tab2.."public var layer"..maps[i][2]..":"..tileMapClass..";\n"
+		fileText = fileText..tab1.."public var layer"..maps[i][2]..":"..tileMapClass..";\n"
 	end
 	fileText = fileText.."\n"
 	
-	fileText = fileText..tab2.."//Sprites\n"
+	fileText = fileText..tab1.."//Sprites\n"
 	for i,v in ipairs(spriteLayers) do
-		fileText = fileText..tab2.."public var "..spriteLayers[i][3].."Group:FlxGroup = new FlxGroup;\n"
+		fileText = fileText..tab1.."public var "..spriteLayers[i][3].."Group:FlxGroup = new FlxGroup();\n"
 	end
 	fileText = fileText.."\n"
 	
 	fileText = fileText.."\n"
-	fileText = fileText..tab2.."public function Level_"..levelName.."(addToStage:Boolean = true, onAddSpritesCallback:Function = null)\n"
-	fileText = fileText..tab2.."{\n"
-	fileText = fileText..tab3.."// Generate maps.\n"
+	fileText = fileText..tab1.."public function Level_"..levelName.."(addToStage:Boolean = true, onAddSpritesCallback:Function = null)\n"
+	fileText = fileText..tab1.."{\n"
+	fileText = fileText..tab2.."// Generate maps.\n"
 	
 	minx = 9999999
 	miny = 9999999
@@ -142,12 +140,8 @@ if exportOnlyCSV == false then
 		layerName = maps[i][2]
 		layer = maps[i][1]
 		
-		fileText = fileText..tab3.."layer"..layerName.." = new "..tileMapClass..";\n"
-		if flixelVersion == "2.5" then
-			fileText = fileText..tab3.."layer"..layerName..".loadMap( new CSV_"..layerName..", Img_"..layerName..", "..as3.tolua(layer.map.tileWidth)..","..as3.tolua(layer.map.tileHeight)..", FlxTilemap.OFF, 0, "..as3.tolua(layer.map.drawIndex)..", "..as3.tolua(layer.map.collideIndex).." );\n"
-		else
-			fileText = fileText..tab3.."layer"..layerName..".loadMap( new CSV_"..layerName..", Img_"..layerName..", "..as3.tolua(layer.map.tileWidth)..","..as3.tolua(layer.map.tileHeight).." );\n"
-		end
+		fileText = fileText..tab2.."layer"..layerName.." = new "..tileMapClass..";\n"
+		fileText = fileText..tab2.."layer"..layerName..".loadMap( new CSV_"..layerName..", Img_"..layerName..", "..as3.tolua(layer.map.tileWidth)..","..as3.tolua(layer.map.tileHeight)..", FlxTilemap.OFF, 0, "..as3.tolua(layer.map.drawIndex)..", "..as3.tolua(layer.map.collideIndex).." );\n"
 		
 		x = as3.tolua(layer.map.x)
 		y = as3.tolua(layer.map.y)
@@ -158,97 +152,80 @@ if exportOnlyCSV == false then
 		if x + width > maxx then maxx = x + width end
 		if y + height > maxy then maxy = y + height end
 		
-		fileText = fileText..tab3.."layer"..layerName..".x = "..string.format("%.6f",x)..";\n"
-		fileText = fileText..tab3.."layer"..layerName..".y = "..string.format("%.6f",y)..";\n"
-		fileText = fileText..tab3.."layer"..layerName..".scrollFactor.x = "..string.format("%.6f",as3.tolua(layer.xScroll))..";\n"
-		fileText = fileText..tab3.."layer"..layerName..".scrollFactor.y = "..string.format("%.6f",as3.tolua(layer.yScroll))..";\n"
-		if flixelVersion ~= "2.5" then
-			fileText = fileText..tab3.."layer"..layerName..".collideIndex = "..as3.tolua(layer.map.collideIndex)..";\n"
-			fileText = fileText..tab3.."layer"..layerName..".drawIndex = "..as3.tolua(layer.map.drawIndex)..";\n"
-		end
+		fileText = fileText..tab2.."layer"..layerName..".x = "..string.format("%.6f",x)..";\n"
+		fileText = fileText..tab2.."layer"..layerName..".y = "..string.format("%.6f",y)..";\n"
+		fileText = fileText..tab2.."layer"..layerName..".scrollFactor.x = "..string.format("%.6f",as3.tolua(layer.xScroll))..";\n"
+		fileText = fileText..tab2.."layer"..layerName..".scrollFactor.y = "..string.format("%.6f",as3.tolua(layer.yScroll))..";\n"
 	end
 	
 	-- Add the layers to the layer list.
 	
-	fileText = fileText.."\n"..tab3.."//Add layers to the master group in correct order.\n"
+	fileText = fileText.."\n"..tab2.."//Add layers to the master group in correct order.\n"
 	fileText = fileText..masterLayerAddText.."\n\n";
 		
 	fileText = fileText..stageAddText
 	
-	fileText = fileText..tab3.."mainLayer = layer"..mainLayer..";\n\n"
-	fileText = fileText..tab3.."boundsMinX = "..minx..";\n"
-	fileText = fileText..tab3.."boundsMinY = "..miny..";\n"
-	fileText = fileText..tab3.."boundsMaxX = "..maxx..";\n"
-	fileText = fileText..tab3.."boundsMaxY = "..maxy..";\n\n"
+	fileText = fileText..tab2.."mainLayer = layer"..mainLayer..";\n\n"
+	fileText = fileText..tab2.."boundsMinX = "..minx..";\n"
+	fileText = fileText..tab2.."boundsMinY = "..miny..";\n"
+	fileText = fileText..tab2.."boundsMaxX = "..maxx..";\n"
+	fileText = fileText..tab2.."boundsMaxY = "..maxy..";\n\n"
 	
-	fileText = fileText..tab2.."}\n\n"	-- end constructor
+	fileText = fileText..tab1.."}\n\n"	-- end constructor
 	
 	
 	
-	baseFileText = baseFileText..tab2.."public var masterLayer:FlxGroup = new FlxGroup;\n\n"
-	baseFileText = baseFileText..tab2.."public var mainLayer:"..tileMapClass..";\n\n"
-	baseFileText = baseFileText..tab2.."public var boundsMinX:int;\n"
-	baseFileText = baseFileText..tab2.."public var boundsMinY:int;\n"
-	baseFileText = baseFileText..tab2.."public var boundsMaxX:int;\n"
-	baseFileText = baseFileText..tab2.."public var boundsMaxY:int;\n\n"
-	baseFileText = baseFileText..tab2.."public function "..baseClassName.."() { }\n\n"
-	if flixelVersion == "2.5" then
-		baseFileText = baseFileText..tab2.."public function addSpriteToLayer(type:Class, group:FlxGroup, x:Number, y:Number, angle:Number, flipped:Boolean, scrollX:Number, scrollY:Number, onAddCallback:Function = null):FlxSprite\n"
-	else
-		baseFileText = baseFileText..tab2.."public function addSpriteToLayer(type:Class, group:FlxGroup, x:Number, y:Number, angle:Number, flipped:Boolean, onAddCallback:Function = null):FlxSprite\n"
-	end
-	baseFileText = baseFileText..tab2.."{\n"
-	baseFileText = baseFileText..tab3.."var obj:FlxSprite = new type(x, y);\n"
-	baseFileText = baseFileText..tab3.."obj.x += obj.offset.x;\n"
-	baseFileText = baseFileText..tab3.."obj.y += obj.offset.y;\n"
-	baseFileText = baseFileText..tab3.."obj.angle = angle;\n"
+	baseFileText = baseFileText..tab1.."public var masterLayer:FlxGroup = new FlxGroup();\n\n"
+	baseFileText = baseFileText..tab1.."public var mainLayer:"..tileMapClass..";\n\n"
+	baseFileText = baseFileText..tab1.."public var boundsMinX:int;\n"
+	baseFileText = baseFileText..tab1.."public var boundsMinY:int;\n"
+	baseFileText = baseFileText..tab1.."public var boundsMaxX:int;\n"
+	baseFileText = baseFileText..tab1.."public var boundsMaxY:int;\n\n"
+	baseFileText = baseFileText..tab1.."public function "..baseClassName.."() { }\n\n"
+	baseFileText = baseFileText..tab1.."public function addSpriteToLayer(type:Class, group:FlxGroup, x:Number, y:Number, angle:Number, flipped:Boolean, scrollX:Number, scrollY:Number, onAddCallback:Function = null):FlxSprite\n"
 	
-	baseFileText = baseFileText..tab3.."// Only override the facing value if the class didn't change it from the default.\n"
-	if flixelVersion == "2.5" then
-		baseFileText = baseFileText..tab3.."if( obj.facing == FlxObject.RIGHT )\n"
-		baseFileText = baseFileText..tab4.."obj.facing = flipped ? FlxObject.LEFT : FlxObject.RIGHT;\n"
-		baseFileText = baseFileText..tab3.."obj.scrollFactor.x = scrollX;\n"
-		baseFileText = baseFileText..tab3.."obj.scrollFactor.y = scrollY;\n"
-		baseFileText = baseFileText..tab3.."group.add(obj);\n"
-	else
-		baseFileText = baseFileText..tab3.."if( obj.facing == FlxSprite.RIGHT )\n"
-		baseFileText = baseFileText..tab4.."obj.facing = flipped ? FlxSprite.LEFT : FlxSprite.RIGHT;\n"
-		baseFileText = baseFileText..tab3.."group.add(obj,true);\n"
-	end
-	baseFileText = baseFileText..tab3.."if(onAddCallback != null)\n"
-	baseFileText = baseFileText..tab4.."onAddCallback(obj, group);\n"
-	baseFileText = baseFileText..tab3.."return obj;\n"
-	baseFileText = baseFileText..tab2.."}\n\n"
+	baseFileText = baseFileText..tab1.."{\n"
+	baseFileText = baseFileText..tab2.."var obj:FlxSprite = new type(x, y);\n"
+	baseFileText = baseFileText..tab2.."obj.x += obj.offset.x;\n"
+	baseFileText = baseFileText..tab2.."obj.y += obj.offset.y;\n"
+	baseFileText = baseFileText..tab2.."obj.angle = angle;\n"
+	
+	baseFileText = baseFileText..tab2.."// Only override the facing value if the class didn't change it from the default.\n"
+	baseFileText = baseFileText..tab2.."if( obj.facing == FlxObject.RIGHT )\n"
+	baseFileText = baseFileText..tab3.."obj.facing = flipped ? FlxObject.LEFT : FlxObject.RIGHT;\n"
+	baseFileText = baseFileText..tab2.."obj.scrollFactor.x = scrollX;\n"
+	baseFileText = baseFileText..tab2.."obj.scrollFactor.y = scrollY;\n"
+	baseFileText = baseFileText..tab2.."group.add(obj);\n"
+	
+	baseFileText = baseFileText..tab2.."if(onAddCallback != null)\n"
+	baseFileText = baseFileText..tab3.."onAddCallback(obj, group);\n"
+	baseFileText = baseFileText..tab2.."return obj;\n"
+	baseFileText = baseFileText..tab1.."}\n\n"
 
 	-- create the sprites.
 	
 	for i,v in ipairs(spriteLayers) do
-		baseFileText = baseFileText..tab2.."public function addSpritesForLayer"..spriteLayers[i][3].."(onAddCallback:Function = null):void { }\n"
+		baseFileText = baseFileText..tab1.."public function addSpritesForLayer"..spriteLayers[i][3].."(onAddCallback:Function = null):void { }\n"
 		layer = spriteLayers[i][2]
-		if flixelVersion == "2.5" then
-			creationText = tab3.."addSpriteToLayer(%class%, "..spriteLayers[i][3].."Group , %xpos%, %ypos%, %degrees%, %flipped%, "..as3.tolua(layer.xScroll)..", "..as3.tolua(layer.xScroll)..", onAddCallback );//%name%\n" 
-		else
-			creationText = tab3.."addSpriteToLayer(%class%, "..spriteLayers[i][3].."Group , %xpos%, %ypos%, %degrees%, %flipped%, onAddCallback );//%name%\n" 
-		end
-		fileText = fileText..tab2.."override public function addSpritesForLayer"..spriteLayers[i][3].."(onAddCallback:Function = null):void\n"
-		fileText = fileText..tab2.."{\n"
+		creationText = tab2.."addSpriteToLayer(%class%, "..spriteLayers[i][3].."Group , %xpos%, %ypos%, %degrees%, %flipped%, "..as3.tolua(layer.xScroll)..", "..as3.tolua(layer.xScroll)..", onAddCallback );//%name%\n" 
+		
+		fileText = fileText..tab1.."override public function addSpritesForLayer"..spriteLayers[i][3].."(onAddCallback:Function = null):void\n"
+		fileText = fileText..tab1.."{\n"
 	
 		fileText = fileText..as3.tolua(DAME.CreateTextForSprites(layer,creationText,"Avatar"))
-		fileText = fileText..tab2.."}\n\n"
+		fileText = fileText..tab1.."}\n\n"
 	end
 	
 	fileText = fileText.."\n"
 
-	fileText = fileText..tab1.."}\n"	-- end class
-	fileText = fileText.."}\n"		-- end package
+	fileText = fileText.."}\n"	-- end class
 	
-	baseFileText = baseFileText..tab1.."}\n"	-- end class
-	baseFileText = baseFileText.."}\n"		-- end package
+	baseFileText = baseFileText.."}\n"	-- end class
 		
 	-- Save the file!
 	
-	DAME.WriteFile(as3Dir.."/Level_"..levelName..".as", fileText )
-	DAME.WriteFile(as3Dir.."/"..baseClassName..".as", baseFileText )
+	DAME.WriteFile(as3Dir.."/Level_"..levelName..".hx", fileText )
+	DAME.WriteFile(as3Dir.."/"..baseClassName..".hx", baseFileText )
 end
 
 
--- flixelSimple_settings.lua
+++ flixelSimple_settings.lua
@@ -4,10 +4,6 @@ DAME.AddHtmlTextLabel("Ensure you use the <b>SimpleClaws</b> PlayState.as file i
 DAME.AddBrowsePath("AS3 dir:","AS3Dir",false, "Where you place the Actionscript files.")
 DAME.AddBrowsePath("CSV dir:","CSVDir",false)
 
-versions = as3.class.Array.new()
-versions.push("2.43")
-versions.push("2.5")
-DAME.AddDropDown("Flixel version:","FlixelVersion",versions,"2.43","Select the version of Flixel relevant to you.");
 DAME.AddTextInput("Base Class", "BaseLevel", "BaseClass", true, "What to call the base class that all levels will extend." )
 DAME.AddTextInput("Level Name", "", "LevelName", true, "The name you wish to call this level." )
 DAME.AddTextInput("Main Layer", "", "MainLayer", true, "Name of the tilemap layer to use as the main layer for hits." )
